f67fc331fc99404fbcf8f2cd114d2c4289dceca7
danila <idanila331@gmail.com>
2026-06-09 14:47:11 +0300

    feat:algorithm euclid

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-'use strict';
+"use strict";
 
+function euclid(firstNumber, secondNumber) {
+  while (secondNumber !== 0) {
+    const remainder = firstNumber % secondNumber;
+
+    firstNumber = secondNumber;
+    secondNumber = remainder;
+  }
+
+  return firstNumber;
+}
+
+console.log(euclid(12, 6));
+console.log(euclid(180, 48));
+console.log(euclid(100, 25));
+console.log(euclid(13, 29));
