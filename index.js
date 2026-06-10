b94f95c924484a699656b2057883fc7e3873db6b
danila <idanila331@gmail.com>
2026-06-10 12:11:03 +0300

    refac: check correctness of values

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 "use strict";
 
 function euclid(firstNumber, secondNumber) {
+  if(firstNumber < 0 || secondNumber < 0) return -1;
+  if(!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) return -1;
   while (secondNumber !== 0) {
     const remainder = firstNumber % secondNumber;
-
-    firstNumber = secondNumber;
-    secondNumber = remainder;
+    [firstNumber,secondNumber] = [secondNumber, remainder];
   }
-
   return firstNumber;
 }
 
 console.log(euclid(12, 6));
 console.log(euclid(180, 48));
-console.log(euclid(100, 25));
+console.log(euclid(-100, 25));
 console.log(euclid(13, 29));
+console.log(euclid(13.8, 29.0));
